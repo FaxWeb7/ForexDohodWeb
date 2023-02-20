b8a3f3549e935318e0ef7c27bafc640bd4e531cb
FaxWeb <80632493+FaxWeb7@users.noreply.github.com>
2023-02-20 23:38:32 +0300

    starting section catalog

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './_global.scss'
 import Header from './components/sections/Header/Header'
 import Home from './components/sections/Home/Home'
+import Catalog from './components/sections/Catalog/Catalog'
 
 const App = () => {
   return (
     <div className='wrapper'>
       <Header />
       <Home />
+      <Catalog />
     </div>
   )
 }
---
+++ src/components/sections/Catalog/Catalog.jsx
@@ -0,0 +1,15 @@
+import './catalog.scss'
+
+const Catalog = () => {
+  return (
+    <section className="catalog">
+      <div className="container">
+        <div className="catalog__inner">
+          Catalog
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Catalog;
