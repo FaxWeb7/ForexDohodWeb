8088b8fe31d111fbb72a3d2363f81c775de50d13
FaxWeb <80632493+FaxWeb7@users.noreply.github.com>
2023-02-24 19:18:21 +0300

    build

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,2 +1,2 @@
-export const CLIENT_PORT = 3000 //443 for https | 80 for http
-export const CLIENT_URL = `http://192.168.1.77:${CLIENT_PORT}`
+export const CLIENT_PORT = 443 //443 for https | 80 for http
+export const CLIENT_URL = `http://forexdohod:${CLIENT_PORT}`
